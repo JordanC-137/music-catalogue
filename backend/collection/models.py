1adeeee1846577b1342872d7df1d9967079a7045
Jordan <jordanasiama3@gmail.com>
2024-08-26 15:46:21 +0100

    Basic models

--- backend/collection/models.py
+++ backend/collection/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Artist(models.Model):
+    name = models.TextField(max_length=200)
+
+class Album(models.Model):
+    title = models.TextField(max_length=200)
+    artist = models.ForeignKey(Artist)
