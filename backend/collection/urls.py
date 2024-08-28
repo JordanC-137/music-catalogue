282424631fb2d47ba5ce37b94c3ed7389db47875
Jordan <jordanasiama3@gmail.com>
2024-08-28 23:16:47 +0100

    Moved from purely json responses to json or html responses

--- backend/collection/urls.py
+++ backend/collection/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
     path('collections/', views.album_list, name="album_list"),
     path('collections/<int:pk>', views.album_detail, name="album_detail"),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- backend/collection/views.py
+++ backend/collection/views.py
@@ -1,40 +1,45 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse, HttpResponse
+
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 
 from collection.models import Album
 from collection.serializers import AlbumSerializer
 
-@csrf_exempt
-def album_list(request):
+@api_view(['GET', 'POST'])
+def album_list(request, format=None):
     if request.method == 'GET':
         albums = Album.objects.all()
         serializer = AlbumSerializer(albums, many = True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
     elif request.method == 'POST':
         data = JSONParser().parse(request)
         serializer = AlbumSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
-    
-def album_detail(request, pk):
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def album_detail(request, pk, format=None):
     try:
         album = Album.objects.get(pk=pk)
     except album.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
     
     if request.method == 'GET':
         serializer = AlbumSerializer(album)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
     elif request.method == 'PUT':
         data = JSONParser().parse(request)
         serializer = AlbumSerializer(album, data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     elif request.method == 'DELETE':
         album.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
