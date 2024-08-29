2b7da32eb5b3adb6e0f66e84627955d2bcc34f09
Jordan <jordanasiama3@gmail.com>
2024-08-29 13:33:02 +0100

    Added year field to album

--- backend/collection/models.py
+++ backend/collection/models.py
@@ -4,12 +4,22 @@ from django.db import models
 class Artist(models.Model):
     name = models.TextField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
 class Album(models.Model):
     title = models.TextField(max_length=200)
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    year = models.DateField(null=True, blank=True)
     period = models.ForeignKey("Period", null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.title
 
 class Period(models.Model):
     name = models.TextField(max_length = 200)
     description = models.TextField()
     color = models.TextField(max_length=6)
+
+    def __str__(self):
+        return self.name
