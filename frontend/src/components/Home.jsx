e8e22a2424302f787ee1f70526acb5644981b916
Jordan <jordanasiama3@gmail.com>
2024-08-30 12:45:02 +0100

    Not yet functional, react no longer getting 404 error so accessing Django. But not managing to read data correctly

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,3 +1,19 @@
-export default function Home(){
+import { useState } from 'react';
+import axios from 'axios';
 
+export default function Home(){
+    const [albums, setAlbums] = useState([]);
+
+    axios.get('http://127.0.0.1:8000/collection/')
+    .then(res => {
+        setAlbums(res.data);
+    })
+    .catch(err => console.log(err));
+
+    const elems = albums.length === 0 ? albums.map(album => <li key={album.id}>{album.title}</li>) : <h1>Nothing</h1>;
+    return (
+        <>
+            { elems }
+        </>
+    );
 }
