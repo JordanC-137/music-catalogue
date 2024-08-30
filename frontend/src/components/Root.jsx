b37a5cf601da9c37d67f80542a81401f9d5826da
Jordan <jordanasiama3@gmail.com>
2024-08-30 12:45:02 +0100

    Not yet functional, react no longer getting 404 error so accessing Django. But not managing to read data correctly

--- frontend/src/components/Root.jsx
+++ frontend/src/components/Root.jsx
@@ -1,7 +1,10 @@
+import { Outlet } from "react-router-dom";
+
 export default function Root(){
     return (
         <>
             <h1>Root component</h1>
+            <Outlet />
         </>
     );
 }
