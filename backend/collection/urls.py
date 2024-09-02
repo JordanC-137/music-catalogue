a825bfb6c9ff3140b18ce0d6d940cf47ec924b59
Jordan <jordanasiama3@gmail.com>
2024-09-02 22:03:59 +0100

    Moved views to use generics

--- backend/collection/urls.py
+++ backend/collection/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('collection/', views.album_list, name="album_list"),
-    path('collection/<int:pk>', views.album_detail, name="album_detail"),
+    path('collection/', views.AlbumList.as_view(), name="album_list"),
+    path('collection/<int:pk>', views.AlbumDetail.as_view(), name="album_detail"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
