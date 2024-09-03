b5a969c448b2dcb4fd6c8fe02c726f14cf50cccc
Jordan <jordanasiama3@gmail.com>
2024-09-03 22:28:44 +0100

    Initial setup for rating model

--- backend/collection/models.py
+++ backend/collection/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models import Q
+from django.conf import settings
 
 # Create your models here.
 class Artist(models.Model):
@@ -23,3 +25,16 @@ class Period(models.Model):
 
     def __str__(self):
         return self.name
+
+class Rating(models.Model):
+    stars = models.IntegerField()
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                condition=models.Q(stars__gte=0) & Q(stars__lte=5),
+                name = "valid_range_of_stars_constraint"
+            )
+        ]
