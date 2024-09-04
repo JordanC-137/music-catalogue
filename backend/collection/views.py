20bee13d06c562fc49b736e91777e74c8baf5685
Jordan <jordanasiama3@gmail.com>
2024-09-05 00:01:56 +0100

    Moving detail page away from generics to allow for custom approach

--- backend/collection/views.py
+++ backend/collection/views.py
@@ -2,10 +2,11 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse, HttpResponse
 
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 
+from django.http import Http404
 from rest_framework import generics
 
 from collection.models import Album
@@ -15,6 +16,9 @@ class AlbumList(generics.ListCreateAPIView):
     queryset = Album.objects.all()
     serializer_class = AlbumSerializer
 
-class AlbumDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Album.objects.all()
-    serializer_class = AlbumSerializer
+class AlbumDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Album.objects.get(pk=pk)
+        except Album.DoesNotExist:
+            raise Http404
