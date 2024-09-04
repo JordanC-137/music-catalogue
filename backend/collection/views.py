ec73d361a05bd36b15b84fbd4255a5255763d903
Jordan <jordanasiama3@gmail.com>
2024-09-05 00:03:01 +0100

    Removed unused imports

--- backend/collection/views.py
+++ backend/collection/views.py
@@ -1,10 +1,5 @@
-from django.views.decorators.csrf import csrf_exempt
-from django.http import JsonResponse, HttpResponse
-
-from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.parsers import JSONParser
 
 from django.http import Http404
 from rest_framework import generics
