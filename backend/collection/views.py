76b67db7ded0839ceecf0a005f3c346687ebf3c1
Jordan <jordanasiama3@gmail.com>
2024-09-05 22:09:45 +0100

    Initial work to move AlbumDetail class away from using generics

--- backend/collection/views.py
+++ backend/collection/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from django.http import Http404
-from rest_framework import generics
+from rest_framework import generics, status
 
 from collection.models import Album
 from collection.serializers import AlbumSerializer
@@ -17,3 +17,17 @@ class AlbumDetail(APIView):
             return Album.objects.get(pk=pk)
         except Album.DoesNotExist:
             raise Http404
+        
+    def get(self, request, pk, format = None):
+        album = self.get_object(pk)
+        serializer = AlbumSerializer(album)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        #Get album and check for relevant rating
+        album = self.get_object(pk)
+
+    def delete(self, request, pk, format=None):
+        album = self.get_object(pk)
+        album.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
