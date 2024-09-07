1f638837c8acd4235e2337b56635ad12493d16c6
Jordan <jordanasiama3@gmail.com>
2024-09-07 20:46:58 +0100

    Get method for Rating List working, only responding with rating's specific to user

--- backend/collection/serializers.py
+++ backend/collection/serializers.py
@@ -10,3 +10,9 @@ class ArtistSerializer(serializers.ModelSerializer):
     class Meta:
         model = Artist
         fields = ['id', 'name']
+
+#TODO currently trying to understand how to serialize foriegn key object for creating new Rating with user pre-selected
+class RatingSerializer(serializers.Serializer):
+    stars = serializers.IntegerField(read_only=True)
+    user_id = serializers.IntegerField(read_only=True)
+    album_id = serializers.IntegerField(read_only=True)
