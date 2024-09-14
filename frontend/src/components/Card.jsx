612546fafcf0657c0dc112265ab3c59eb789b292
Jordan <jordanasiama3@gmail.com>
2024-09-14 22:59:03 +0100

    Expanding card to include image

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
 
 export default function Card({value}){
+    const imgPath = `../images/album_${value.id}.jpg`;
     return (
         <Link to={`collection/${value.id}`}>
         <div className = 'card-div'>
+            <img className="thumbnail" src={imgPath}></img>
             <h3>{ value.title }</h3>
         </div>
         </Link>
