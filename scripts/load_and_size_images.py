910d0b32421eb94740cd47d2167c5938d9261060
Jordan <jordanasiama3@gmail.com>
2024-09-21 20:58:43 +0100

    Some pre-processing image work

--- scripts/load_and_size_images.py
+++ scripts/load_and_size_images.py
@@ -2,11 +2,13 @@ from PIL import Image
 from urllib.request import urlopen
 import argparse
 
-HEIGHT, WIDTH = (200, 200)
+NEW_DIMENSIONS = (200, 200)
 url = ""
 #img = Image.open(urlopen(url))
-def resize_and_place_img(url=None, output_path='./new_image.jpg'):
-    pass
+def resize_and_place_img(url, output_path='new_image.jpg'):
+    img = Image.open(urlopen(url))
+    img.resize(NEW_DIMENSIONS)
+    img.save(f"../frontend/images/{output_path}")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="""Script for downloading
@@ -14,4 +16,6 @@ if __name__ == '__main__':
                                     into consistent dimensions""")
     parser.add_argument('url', help="URl to grab initial image from")
     parser.add_argument('output_path', help="Path for final output image")
-    parser.parse_args()
+    args = parser.parse_args()
+
+    resize_and_place_img(args.url, args.output_path)
