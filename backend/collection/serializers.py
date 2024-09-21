e99a41cbc6818cefe16e129c99478be889acb5cc
Jordan <jordanasiama3@gmail.com>
2024-09-21 14:24:53 +0100

    Removed old statement

--- backend/collection/serializers.py
+++ backend/collection/serializers.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class AlbumSerializer(serializers.ModelSerializer):
     class Meta:
         model = Album
-        fields = ['id', 'title', 'artist', 'rym_rating']
+        fields = ['id', 'title', 'artist']
 
 class ArtistSerializer(serializers.ModelSerializer):
     class Meta:
