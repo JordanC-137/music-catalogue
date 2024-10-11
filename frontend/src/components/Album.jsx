13eb81d67b422e8709e765f4a5f1f953f482566c
Jordan <jordanasiama3@gmail.com>
2024-10-11 17:08:21 +0100

    Including image in album page

--- frontend/src/components/Album.jsx
+++ frontend/src/components/Album.jsx
@@ -8,8 +8,11 @@ export async function loader({params}){
 
 export default function Album(){
     const album = useLoaderData();
+    const imgPath = `../images/album_${album.id}.jpg`;
     return (
         <>
+            <img className="mainImage" src={imgPath}></img>
+
             <h1>{album.title}</h1>
         </>
     );
